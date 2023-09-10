0cbe4327f4ffab49e8ee67f37a1f6aed296018bf
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2023-09-10 02:05:45 -0300

    practica_relaciones

---
+++ Clase01/ejercicio01/src/main/java/com/um/ejercicio01/repositorios/PersonaRepository.java
@@ -0,0 +1,9 @@
+package com.um.ejercicio01.repositorios;
+
+import com.um.ejercicio01.entidades.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonaRepository extends JpaRepository<Persona,Long> {
+}
