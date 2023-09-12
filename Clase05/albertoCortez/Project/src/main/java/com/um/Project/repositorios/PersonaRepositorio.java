afb2872226ae6b034f9f94905e871ca45a035116
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2023-09-12 14:02:11 -0300

    dummy_controller

--- Clase05/albertoCortez/Project/src/main/java/com/um/Project/repositorios/PersonaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.um.Project.repositorios;
-
-import com.um.Project.entidades.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepositorio extends JpaRepository<Persona, Long> {
-}
